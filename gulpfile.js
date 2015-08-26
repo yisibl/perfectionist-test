7e932bc6e113a5e922021dbf8fd1251855bfa130
一丝 <percyley@qq.com>
2015-08-26 15:05:01 +0800

    add @apply rule

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,33 @@
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer-core')
+var gulp          = require('gulp')
+var cssfmt        = require('cssfmt')
+var postcss       = require('gulp-postcss')
+var prettify      = require('gulp-jsbeautifier')
 var perfectionist = require('perfectionist')
-var cssfmt = require('cssfmt')
-  //npm  install js-beautify
-var prettify = require('gulp-jsbeautifier');
+var autoprefixer  = require('autoprefixer-core')
 
 var perfectionistOptions = {
   maxValueLength: 90
 }
+
 var browserOptions = ["> 0%"]
 
-gulp.task('default', function() {
-  return gulp.src('input/*.css')
+gulp.task('test', function() {
+  return gulp.src('input/source-map.scss')
     .pipe(postcss([
       autoprefixer({
         browsers: browserOptions
       }),
       perfectionist(perfectionistOptions)
     ]))
-    .pipe(gulp.dest('output/'))
+    .pipe(gulp.dest('test/'))
 })
 
 gulp.task('perfectionist', function() {
-  return gulp.src('input/source-map.scss')
+  return gulp.src('input/*.css')
     .pipe(postcss([
       perfectionist(perfectionistOptions)
     ]))
-    .pipe(gulp.dest('output/'))
+    .pipe(gulp.dest('perfectionist/'))
 })
 
 gulp.task('cssfmt', function() {
@@ -45,7 +45,8 @@ gulp.task('prettify', function() {
       selector_separator_newline: false,
       // newline_between_rules: false
     }))
-    .pipe(gulp.dest('prettify/'));
-});
+    .pipe(gulp.dest('prettify/'))
+})
 
-gulp.watch('input/*.css', ['default', 'cssfmt', 'prettify'])
+gulp.watch('input/*.css', ['perfectionist', 'cssfmt', 'prettify'])
+gulp.task('default', ['perfectionist', 'cssfmt', 'prettify'])
